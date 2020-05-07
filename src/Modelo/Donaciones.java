2280571f7b7c582978e390e9e5d79231644c744d
Ivanhoyas <ivanhoyasfernandez@gmail.com>
2020-05-07 20:06:10 +0200

    Definición de donante y donación en sus respectivas clases, cambio de nombre de Formulariodeincripcion y formularios siguiendo el criterio del anterior commit

--- src/Modelo/Donaciones.java
+++ src/Modelo/Donaciones.java
@@ -1,5 +1,50 @@
 package Modelo;
 
+import java.sql.Date;
+
 public class Donaciones {
+	private String Codigo;
+	private Date Fecha;
+	private String Volumen;
+	private String Tipo;
+	private Date UltDonacion;
+	public Donaciones(String codigo, Date fecha, String volumen, String tipo, Date ultDonacion) {
+		super();
+		Codigo = codigo;
+		Fecha = fecha;
+		Volumen = volumen;
+		Tipo = tipo;
+		UltDonacion = ultDonacion;
+	}
+	public String getCodigo() {
+		return Codigo;
+	}
+	public void setCodigo(String codigo) {
+		Codigo = codigo;
+	}
+	public Date getFecha() {
+		return Fecha;
+	}
+	public void setFecha(Date fecha) {
+		Fecha = fecha;
+	}
+	public String getVolumen() {
+		return Volumen;
+	}
+	public void setVolumen(String volumen) {
+		Volumen = volumen;
+	}
+	public String getTipo() {
+		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+	public Date getUltDonacion() {
+		return UltDonacion;
+	}
+	public void setUltDonacion(Date ultDonacion) {
+		UltDonacion = ultDonacion;
+	}
 
 }
--- src/Modelo/Donantes.java
+++ src/Modelo/Donantes.java
@@ -1,5 +1,161 @@
 package Modelo;
 
+import java.sql.Blob;
+import java.sql.Date;
+
 public class Donantes {
 
-}
+	private String NumDonante;
+	private String Nombre;
+	private String Apellido1;
+	private String Apellido2;
+	private String DNI_NIE;
+	private Date FechaNac;
+	private int Tlf;
+	private int TLFMovil;
+	private String Email;
+	private char Sexo;
+	private String TipoSang;
+	private String Direccion;
+	private String Residencia;
+	private String Provincia;
+	private String Pobacion;
+	private int CP;
+	private String Pais;
+	private Blob Foto;
+	public Donantes(String numDonante, String nombre, String apellido1, String apellido2, String dNI_NIE, Date fechaNac,
+			int tlf, int tLFMovil, String email, char sexo, String tipoSang, String direccion, String residencia,
+			String provincia, String pobacion, int cP, String pais, Blob foto) {
+		super();
+		NumDonante = numDonante;
+		Nombre = nombre;
+		Apellido1 = apellido1;
+		Apellido2 = apellido2;
+		DNI_NIE = dNI_NIE;
+		FechaNac = fechaNac;
+		Tlf = tlf;
+		TLFMovil = tLFMovil;
+		Email = email;
+		Sexo = sexo;
+		TipoSang = tipoSang;
+		Direccion = direccion;
+		Residencia = residencia;
+		Provincia = provincia;
+		Pobacion = pobacion;
+		CP = cP;
+		Pais = pais;
+		Foto = foto;
+	}
+	public String getNumDonante() {
+		return NumDonante;
+	}
+	public void setNumDonante(String numDonante) {
+		NumDonante = numDonante;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido1() {
+		return Apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		Apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return Apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		Apellido2 = apellido2;
+	}
+	public String getDNI_NIE() {
+		return DNI_NIE;
+	}
+	public void setDNI_NIE(String dNI_NIE) {
+		DNI_NIE = dNI_NIE;
+	}
+	public Date getFechaNac() {
+		return FechaNac;
+	}
+	public void setFechaNac(Date fechaNac) {
+		FechaNac = fechaNac;
+	}
+	public int getTlf() {
+		return Tlf;
+	}
+	public void setTlf(int tlf) {
+		Tlf = tlf;
+	}
+	public int getTLFMovil() {
+		return TLFMovil;
+	}
+	public void setTLFMovil(int tLFMovil) {
+		TLFMovil = tLFMovil;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public char getSexo() {
+		return Sexo;
+	}
+	public void setSexo(char sexo) {
+		Sexo = sexo;
+	}
+	public String getTipoSang() {
+		return TipoSang;
+	}
+	public void setTipoSang(String tipoSang) {
+		TipoSang = tipoSang;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getResidencia() {
+		return Residencia;
+	}
+	public void setResidencia(String residencia) {
+		Residencia = residencia;
+	}
+	public String getProvincia() {
+		return Provincia;
+	}
+	public void setProvincia(String provincia) {
+		Provincia = provincia;
+	}
+	public String getPobacion() {
+		return Pobacion;
+	}
+	public void setPobacion(String pobacion) {
+		Pobacion = pobacion;
+	}
+	public int getCP() {
+		return CP;
+	}
+	public void setCP(int cP) {
+		CP = cP;
+	}
+	public String getPais() {
+		return Pais;
+	}
+	public void setPais(String pais) {
+		Pais = pais;
+	}
+	public Blob getFoto() {
+		return Foto;
+	}
+	public void setFoto(Blob foto) {
+		Foto = foto;
+	}
+	
+	
+	
+	
+	}
